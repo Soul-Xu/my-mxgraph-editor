6f2c00e71ae7a85fc7476afb9fe7ff547592347a
leonxu0207 <741930067@qq.com>
2024-02-04 13:09:03 +0800

    feat: init

---
+++ demo/toolbar.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { message, Tooltip, Icon } from 'antd';
+
+import './toolbar.less';
+
+const userGuideText = 'group the cells selected';
+
+message.config({
+  top: 60,
+  duration: 2,
+  maxCount: 3,
+});
+
+class Toolbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scale: null
+    };
+  }
+
+  componentWillMount() {
+    
+  }
+
+  componentDidMount() {
+    const { editor } = this.props;
+    const graph = editor && editor.graph;
+
+    if (graph) {
+      // 添加缩放事件监听器
+      graph.addListener(mxEvent.ZOOM, (sender, evt) => {
+        // 在这里可以获取当前的缩放比例，并做相应的操作
+        const scale = graph.view.scale;
+
+        // 例如，可以将缩放比例打印到控制台
+        console.log('当前缩放比例:', scale);
+        this.setState({ scale }); // 使用 setState 更新组件的状态
+      });
+    }
+  }
+
+  render() {
+    const {
+      editor, updateDiagramData,
+    } = this.props;
+
+    const graph = editor && editor.graph;
+    const { scale } = this.state;
+  
+    // 将缩放比例转换成百分比形式
+    const scalePercentage = Math.round(scale * 100) + '%';
+
+    return [
+      <div key="toolbar" className="toolbar">
+        <div
+          className="toolbar-btn"
+          onClick={() => {
+            graph.zoomIn();
+          }}
+        >
+          <Icon type="plus" />
+        </div>
+        <div className="toolbar-process">
+          <span>{scalePercentage}</span>
+        </div>
+        <div
+          className="toolbar-btn"
+          onClick={() => {
+            graph.zoomOut();
+          }}
+        >
+          <Icon type="minus" />
+        </div>
+        <div
+          className="toolbar-btn"
+          onClick={() => {
+            graph.zoomActual();
+          }}
+        >
+          <Icon type="redo" />
+        </div>
+      </div>,
+    ];
+  }
+}
+
+Toolbar.propTypes = {
+  updateDiagramData: PropTypes.func,
+};
+
+// Specifies the default values for props:
+Toolbar.defaultProps = {
+  updateDiagramData: () => {},
+};
+
+export default Toolbar;
+
